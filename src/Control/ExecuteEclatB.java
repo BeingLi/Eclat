46b6cbb8e949a38024e521e05ef06b5b04e879fa
http1992215 <1320573594@qq.com>
2018-09-21 14:11:20 +0800

    eclat算法代码实现

---
+++ src/Control/ExecuteEclatB.java
@@ -0,0 +1,24 @@
+package Control;
+
+import algorithm.Eclat;
+
+public class ExecuteEclatB {
+	public static String fileSrcWrite="D:/Result1.txt";
+	public static String fileSrcRead="./Data1.txt";
+	public static String SPLIT = ";";
+	public static int minSuport = 2;
+	
+
+	public static void main(String[] args) {
+		long begin=System.currentTimeMillis();
+
+		Eclat eclat = new Eclat(fileSrcRead, SPLIT, minSuport);
+		eclat.mainFlow_B(fileSrcWrite);
+		long end=System.currentTimeMillis();
+		
+		double time=(double)(end-begin)/1000;
+		eclat.writeResult2File("执行Eclat算法共耗时"+time+"秒", fileSrcWrite);
+		System.out.println("执行Eclat算法共耗时"+time+"秒");
+	}
+
+}
